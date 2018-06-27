6a696882ff72b6297542bc158b0779b222603b8b
Leonardo Pricevicius <leonardo@okn.com.br>
2018-06-27 10:53:04 -0300

    ajuste busca nome

--- inc/render.php
+++ inc/render.php
@@ -1,5 +1,9 @@
 <?php
 
+    function tirarAcentos($string){
+        return preg_replace(array("/(á|à|ã|â|ä)/","/(Á|À|Ã|Â|Ä)/","/(é|è|ê|ë)/","/(É|È|Ê|Ë)/","/(í|ì|î|ï)/","/(Í|Ì|Î|Ï)/","/(ó|ò|õ|ô|ö)/","/(Ó|Ò|Õ|Ô|Ö)/","/(ú|ù|û|ü)/","/(Ú|Ù|Û|Ü)/","/(ñ)/","/(Ñ)/"),explode(" ","a A e E i I o O u U n N"),$string);
+    }
+
     /* *
         Carrega a planilha!
     */
@@ -16,21 +20,26 @@
     }
     unset($return['PARTICIPANTES'][1]);
     $arrParticipantes = array();
+    $a=0;
     foreach($return['PARTICIPANTES'] as $participante){
-        $arrParticipantes[] = $participante['B'];
+        $arrParticipantes[$a] = $participante['B'];
+        $arrParticipantesAcentos[$a] = tirarAcentos($participante['B']);
+        $a++;
     }
-
+    $postNome = mb_strtoupper(urldecode($_POST['nome']),'UTF-8');
+    
     /* Consulta se o nome esta na planilha */
-    if (in_array(urldecode($_POST['nome']), $arrParticipantes)) { 
+    if (in_array($postNome, $arrParticipantes) || in_array($postNome, $arrParticipantesAcentos) ) { 
+        $thisName = array_search(tirarAcentos($postNome),$arrParticipantesAcentos);
         $image = ImageCreateFromJPEG(__DIR__."/Participante_Certificado_BIGDATAGA.jpg"); 
         $white = ImageColorAllocate($image, 255, 255, 255); 
         $black = ImageColorAllocate($image, 46, 29, 29); 
         ImageFill($image, 35, 0, $white); 
         $font = __DIR__.'/OpenSans-Bold.ttf';
 
-        ImageTTFText($image,45, 0, 670, 1523, $black, $font, urldecode($_POST['nome']));
+        ImageTTFText($image,45, 0, 670, 1523, $black, $font, $arrParticipantes[$thisName]);
         header("Content-Type: image/jpeg"); 
-        header('Content-Disposition: attachment; filename="'.$_POST['nome'].'.jpg"');
+        header('Content-Disposition: attachment; filename="'.$arrParticipantesAcentos[$thisName].'.jpg"');
         ImageJpeg($image); 
     }else{
         echo "
